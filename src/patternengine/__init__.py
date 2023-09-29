a310e356b1b1b12b0115af9867941ff4d202cf93
Michael Lamertz <michael.lamertz@gmail.com>
2023-09-29 20:59:13 +0200

    Import POMS and mutators into patternengine namespace

--- src/patternengine/__init__.py
+++ src/patternengine/__init__.py
@@ -4,3 +4,4 @@ import patternengine.bullets  # noqa: F401
 
 from patternengine.engine import *  # noqa: F401, F403
 from patternengine.bullet import *  # noqa: F401, F403
+from patternengine.poms import *  # noqa: F401, F403
